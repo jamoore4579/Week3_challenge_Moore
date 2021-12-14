a449c98c7679ad4fc681002170c013d80d10265b
Irish Moore <jasonamoore@frontier.com>
2021-12-14 00:22:41 -0500

    add array var values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +19,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Numeric Characters Array
+var numCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Lowercase Characters Array
+var lowerCaseCharacters = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Uppercase Characters Array
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Special Characters Array
+var specCharacters = ["@", "(", ")", "{","}", "[", "]", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", "~", "-", "_", "."]
+
